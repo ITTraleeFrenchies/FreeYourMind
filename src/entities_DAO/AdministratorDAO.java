c833738e4fe8e3c09cbf1d4543e6e265a4411895
kyalha <angel079@hotmail.fr>
2014-11-20 16:40:32 +0000

    Creation of Entities_DAO and the interface AdministratorDAO.java

---
+++ src/entities_DAO/AdministratorDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entities_DAO;
+
+/**
+ *
+ * @author t00178747
+ */
+public interface AdministratorDAO {
+    
+}
