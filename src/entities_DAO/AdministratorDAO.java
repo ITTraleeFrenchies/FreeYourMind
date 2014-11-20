b4f92a0b6368899c8c53b90d7da2013676cd9e28
kyalha <angel079@hotmail.fr>
2014-11-20 16:55:06 +0000

    Create the real AdministratorDAO.java

--- src/entities_DAO/AdministratorDAO.java
+++ src/entities_DAO/AdministratorDAO.java
@@ -6,10 +6,42 @@
 
 package entities_DAO;
 
+import entities.Administrator;
+import java.util.List;
+
 /**
  *
  * @author t00178747
  */
-public interface AdministratorDAO {
+public class AdministratorDAO implements DAO{
+
+    @Override
+    public Administrator find(long id) {
+        return null;
+    }
+
+    @Override
+    public Administrator create(Object obj) {
+        return null;
+    }
+
+    @Override
+    public Administrator update(Object obj) {
+        return null;
+    }
+
+    @Override
+    public void delete(Object obj) {
+    }
+
+    @Override
+    public List<Administrator> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Administrator> findById() {
+        return null;
+    }
     
 }
