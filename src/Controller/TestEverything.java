be8aef1a3d49cea9ef16c81d5425d658659c11a6
t00178747 <t00178747@1XX782BCBAC2827.students.ittralee.local>
2014-11-26 17:59:25 +0000

    Update MemberDAO

--- src/Controller/TestEverything.java
+++ src/Controller/TestEverything.java
@@ -5,8 +5,10 @@
  */
 package Controller;
 
+import entities.Member;
 import entities_DAO.MemberDAO;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,11 +21,22 @@ public class TestEverything {
     
 
     public static void main(String[] args) {
-        ConnectionDatabaseManager connect = new ConnectionDatabaseManager();
-        MemberDAO memberDAO= new MemberDAO(connect);
-       
-        memberDAO.find("T10000000");
-        connect.closeConnection();
+       // ConnectionDatabaseManager connect = new ConnectionDatabaseManager();
         
+        MemberDAO memberDAO= new MemberDAO();
+        // -------------- TEST FOR FINDALL ----------------
+       /* List<Member> members =  memberDAO.findAll();
+        for (Member member : members){
+            System.out.println(member.getTnumber());
+        }
+        */
+        // -------------- TEST FOR FINDBYTNUMBER ----------------
+        /*
+        Member member = memberDAO.findByTnumber("T10000000");
+        System.out.println(member.getNickname());
+                */
+        // -------------- TEST FOR CREATE ----------------
+        Member member = new Member("T0000012","nicknamemember3","password3",null,null,null,"addr3@gmail.com",null,null,null,null,null);
+        System.out.println(memberDAO.create(member));
     }
 }
--- src/entities_DAO/MemberDAO.java
+++ src/entities_DAO/MemberDAO.java
@@ -8,11 +8,15 @@ package entities_DAO;
 import Controller.ConnectionDatabaseManager;
 import entities.Member;
 import java.awt.Image;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -20,47 +24,94 @@ import java.util.logging.Logger;
  */
 public class MemberDAO {
 
-    private ConnectionDatabaseManager connection;
+    private String nameDriver = "jdbc:oracle:thin:@cp3dbinstance.c4pxnpz4ojk8.us-east-1.rds.amazonaws.com:1521:cp3db";
+    private String username = "mm3";
+    private String password = "mm3";
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
 
-    public MemberDAO(ConnectionDatabaseManager connection) {
-        this.connection = connection;
+    public MemberDAO() {
+
+    }
+
+    public void openConnection() {
         try {
-            connection.connectionDatabase();
+            this.connection = DriverManager.getConnection(nameDriver, username, password);
+            this.statement = connection.createStatement();
         } catch (SQLException ex) {
-            Logger.getLogger(MemberDAO.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getErrorCode() + " : error with the openning of  connection. ");
         }
     }
 
-    public Member find(String tnumber) {
-        Member member =null;
-        Image profile_pic = null;
-        System.out.println(tnumber);
+    public void closeConnection() {
         try {
-            ResultSet result = this.connection.getConnection().createStatement().executeQuery("SELECT * FROM MEMBER WHERE TNUMBER = " + tnumber);
-            if (result.first()) {
+            if (this.connection != null && this.statement != null && this.resultSet != null) {
+                this.connection.close();
+                this.statement.close();
+                this.resultSet.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getErrorCode() + " : error with the closing of connection. ");
+        }
+
+    }
+
+    public Member find(String tnumber) {
+        Member member = null;
+        Image profile_pic = null;
+
+        openConnection();
+        try {
+            // ResultSet result = this.connection.getConnection().createStatement().executeQuery("SELECT * FROM MEMBER WHERE TNUMBER = " + tnumber);
+            this.resultSet = statement.executeQuery("SELECT * FROM MEMBER WHERE TNUMBER = '" + tnumber+"';");
+            if (this.resultSet.first()) {
                 member = new Member(
                         tnumber,
-                        result.getString("nickname"),
-                        result.getString("password"),
-                        result.getString("firstname"),
-                        result.getString("surname"),
-                        result.getDate("date_birth"),
-                        result.getString("email_addr"),
-                        result.getString("street"),
-                        result.getString("city"),
-                        result.getString("county"),
-                        result.getDate("subscription_date"),
+                        this.resultSet.getString("nickname"),
+                        this.resultSet.getString("password"),
+                        this.resultSet.getString("firstname"),
+                        this.resultSet.getString("surname"),
+                        this.resultSet.getDate("date_birth"),
+                        this.resultSet.getString("email_addr"),
+                        this.resultSet.getString("street"),
+                        this.resultSet.getString("city"),
+                        this.resultSet.getString("county"),
+                        this.resultSet.getDate("subscription_date"),
                         profile_pic
                 );
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        closeConnection();
+
         return member;
     }
 
-    public Member create(Member member) {
-        return null;
+    public Member create(Member memberToCreate) {
+        Member member = memberToCreate;
+        String sql = " INSERT INTO MEMBER(TNUMBER,NICKNAME,PASSWORD,FIRSTNAME,"
+                + "SURNAME,DATE_BIRTH,EMAIL_ADDR,STREET,CITY,COUNTY,SUBSCRIPTION_DATE,"
+                + "PROFILE_PIC) VALUES('" + memberToCreate.getTnumber() +"','" 
+                + memberToCreate.getNickname() +"','"+memberToCreate.getPassword()+"','"
+                + memberToCreate.getFirstname() +"','"+memberToCreate.getSurname()+"','"
+                + memberToCreate.getDate_birth() +"','" +memberToCreate.getEmail_addr()+"','"
+                + memberToCreate.getStreet()+"','"+memberToCreate.getCity()+"','"
+                + memberToCreate.getCounty()+"','"+memberToCreate.getSurbscription_date()+"','"
+                + memberToCreate.getProfile_pic()+"');";
+        
+        System.out.println(sql);
+        openConnection();
+        try {
+            this.resultSet = statement.executeQuery(sql);
+
+        } catch (SQLException ex) {
+             ex.printStackTrace();
+        }
+        closeConnection();
+
+        return member;
     }
 
     public Member update(Member member) {
@@ -72,10 +123,66 @@ public class MemberDAO {
     }
 
     public List<Member> findAll() {
-        return null;
+        List<Member> members = new ArrayList();
+        Member member = null;
+        String sql = "SELECT * FROM MEMBER";
+
+        openConnection();
+        try {
+            this.resultSet = statement.executeQuery(sql);
+            while (this.resultSet.next()) {
+                member = new Member(
+                        this.resultSet.getString("tnumber"),
+                        this.resultSet.getString("nickname"),
+                        this.resultSet.getString("password"),
+                        this.resultSet.getString("firstname"),
+                        this.resultSet.getString("surname"),
+                        this.resultSet.getDate("date_birth"),
+                        this.resultSet.getString("email_addr"),
+                        this.resultSet.getString("street"),
+                        this.resultSet.getString("city"),
+                        this.resultSet.getString("county"),
+                        this.resultSet.getDate("subscription_date"),
+                        null);
+                members.add(member);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getErrorCode() + " error with the sql request.");
+        }
+        closeConnection();
+
+        return members;
     }
 
-    public List<Member> findById() {
-        return null;
+    public Member findByTnumber(String tnumber) {
+
+        Member member = null;
+        String sql = "SELECT * FROM MEMBER WHERE TNUMBER = '" + tnumber+ "'";
+
+        openConnection();
+        try {
+            this.resultSet = statement.executeQuery(sql);
+            while (this.resultSet.next()) {
+                member = new Member(
+                        this.resultSet.getString("tnumber"),
+                        this.resultSet.getString("nickname"),
+                        this.resultSet.getString("password"),
+                        this.resultSet.getString("firstname"),
+                        this.resultSet.getString("surname"),
+                        this.resultSet.getDate("date_birth"),
+                        this.resultSet.getString("email_addr"),
+                        this.resultSet.getString("street"),
+                        this.resultSet.getString("city"),
+                        this.resultSet.getString("county"),
+                        this.resultSet.getDate("subscription_date"),
+                        null);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getErrorCode() + " error with the sql request.");
+        }
+        closeConnection();
+
+        return member;
     }
 }
