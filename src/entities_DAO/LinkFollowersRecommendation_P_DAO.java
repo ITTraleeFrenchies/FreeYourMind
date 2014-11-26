1985c8555600952e99a6aea8d09159f2d62f7b32
t00178764 <t00178764@1XXB8CA3A80B753.students.ittralee.local>
2014-11-26 10:17:04 +0000

    LinkFollowersRecommendation_M_DAO and LinkFollowersRecommendation_P_DAO added to entites_DAO

---
+++ src/entities_DAO/LinkFollowersRecommendation_P_DAO.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entities_DAO;
+
+import entities.LinkFollowersRecommendation_P;
+import java.util.List;
+/**
+ *
+ * @author t00178764
+ */
+
+
+
+public class LinkFollowersRecommendation_P_DAO {
+       public LinkFollowersRecommendation_P find(long id) {
+        return null;
+    }
+
+    public LinkFollowersRecommendation_P create(LinkFollowersRecommendation_P link) {
+        return null;
+    }
+
+    public LinkFollowersRecommendation_P update(LinkFollowersRecommendation_P link) {
+        return null;
+    }
+
+    public void delete(LinkFollowersRecommendation_P link) {
+    }
+
+    public List<LinkFollowersRecommendation_P> findAll() {
+        return null;
+    }
+
+    public List<LinkFollowersRecommendation_P> findById() {
+        return null;
+    }
+}
