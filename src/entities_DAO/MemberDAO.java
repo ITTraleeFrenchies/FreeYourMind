5bc8dd13b15d13d6d10aa90f94c0a02291e78347
t00178747 <t00178747@1XXB8CA3A8A6F8C.students.ittralee.local>
2014-11-26 09:29:12 +0000

    Adding BlockedDAO and FollowDAO

---
+++ src/entities_DAO/MemberDAO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entities_DAO;
+
+import entities.Member;
+import java.util.List;
+
+/**
+ *
+ * @author t00178747
+ */
+public class MemberDAO {
+      public Member find(int id) {
+        return null;
+    }
+
+    public Member create(Member obj) {
+        return null;
+    }
+
+    public Member update(Member obj) {
+        return null;
+    }
+
+    public Member delete(Member obj) {
+          return null;
+    }
+
+    public List<Member> findAll() {
+        return null;
+    }
+
+    public List<Member> findById() {
+        return null;
+    }
+}
