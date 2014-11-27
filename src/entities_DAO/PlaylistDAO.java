d6f44754991ab73c2157815493e1854f8ea3a719
Mickael L'Haridon <>
2014-11-27 14:30:39 +0000

    All Methods of PlaylistDAO Class (last commit was MusicDAO by the way not Music Class)

--- src/entities_DAO/PlaylistDAO.java
+++ src/entities_DAO/PlaylistDAO.java
@@ -6,10 +6,148 @@
 
 package entities_DAO;
 
+import entities.Playlist;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author t00178760
  */
 public class PlaylistDAO {
+    private String nameDriver = "jdbc:oracle:thin:@cp3dbinstance.c4pxnpz4ojk8.us-east-1.rds.amazonaws.com:1521:cp3db";
+    private String username = "mm3";
+    private String password = "mm3";
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
     
+        public void openConnection() {
+        try {
+            this.connection = DriverManager.getConnection(nameDriver, username, password);
+            this.statement = connection.createStatement();
+        } catch (SQLException ex) {
+            System.out.println(ex.getErrorCode() + " : error with the openning of  connection. ");
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            if (this.connection != null && this.statement != null && this.resultSet != null) {
+                this.connection.close();
+                this.statement.close();
+                this.resultSet.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getErrorCode() + " : error with the closing of connection. ");
+        }
+
+    }
+    
+    public Playlist find(String id) {
+        Playlist playlist = null;
+
+        openConnection();
+        try {
+            this.resultSet = statement.executeQuery("SELECT * FROM PLAYLIST WHERE ID_PLAYLIST = '" + id +"';");
+            if (this.resultSet.first()) {
+                playlist = new Playlist(
+                        id,
+                        this.resultSet.getInt("id_library"),
+                        this.resultSet.getString("name"),
+                        this.resultSet.getDate("creation_date"),
+                        this.resultSet.getBoolean("hidden")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        closeConnection();
+
+        return playlist;
+    }
+    
+    public Playlist create(Playlist newPlaylist) {
+        Playlist playlist = newPlaylist;
+        String sql = " INSERT INTO PLAYLIST(ID_PLAYLIST,ID_LIBRARY,NAME,CREATION_DATE,HIDDEN"
+                + " VALUES('" + newPlaylist.getIDPlaylist() +"','" 
+                + newPlaylist.getIDLibrary() +"','"+newPlaylist.getName()+"','"
+                + newPlaylist.getCreationDate() +"','"+newPlaylist.isHidden()+"');";
+        
+        System.out.println(sql);
+        openConnection();
+        try {
+            this.resultSet = statement.executeQuery(sql);
+
+        } catch (SQLException ex) {
+             ex.printStackTrace();
+        }
+        closeConnection();
+
+        return playlist;
+    }
+    
+    public Playlist update(Playlist upPlaylist) {
+        String sql = "UPDATE PLAYLIST "
+                +"SET ID_PLAYLIST='"+upPlaylist.getIDPlaylist()+"',"
+                +"SET ID_LIBRARY='"+upPlaylist.getIDLibrary()+"',"
+                +"SET NAME='"+upPlaylist.getName()+"',"
+                +"SET CREATION_DATE='"+upPlaylist.getCreationDate()+"',"
+                +"SET HIDDEN='"+upPlaylist.isHidden()+"',;";
+        
+        System.out.println(sql);
+        openConnection();
+        try {
+            this.resultSet = statement.executeQuery(sql);
+
+        } catch (SQLException ex) {
+             ex.printStackTrace();
+        }
+        closeConnection();
+
+        return upPlaylist;
+    }
+    
+    public void delete(Playlist playlist) {
+        String sql = "DELETE FROM PLAYLIST WHERE ID_PLAYLIST = '"+playlist.getIDPlaylist()+"';";
+        
+        openConnection();
+        try {
+            this.resultSet = statement.executeQuery(sql);
+        }catch (SQLException ex) {
+            System.out.println(ex.getErrorCode() + " error with the sql request.");
+        }
+    }
+    
+    public List<Playlist> findAll() {
+        List<Playlist> playlists = new ArrayList();
+        Playlist playlist = null;
+        String sql = "SELECT * FROM PLAYLIST";
+
+        openConnection();
+        try {
+            this.resultSet = statement.executeQuery(sql);
+            while (this.resultSet.next()) {
+                playlist = new Playlist(
+                        this.resultSet.getString("id_playlist"),
+                        this.resultSet.getInt("id_library"),
+                        this.resultSet.getString("name"),
+                        this.resultSet.getDate("creation_date"),
+                        this.resultSet.getBoolean("hidden")
+                        );
+                playlists.add(playlist);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getErrorCode() + " error with the sql request.");
+        }
+        closeConnection();
+
+        return playlists;
+    }
 }
