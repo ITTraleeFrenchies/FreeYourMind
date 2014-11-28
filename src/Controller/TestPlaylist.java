a8e8d37113381e845389514cf3dc4cacb9c7fa09
t00178764 <t00178764@1XX782BCBAC22B8.students.ittralee.local>
2014-11-28 13:55:15 +0000

    Creation of TestPlaylist. Note : it's full of shit, don't run it

---
+++ src/Controller/TestPlaylist.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import entities.Playlist;
+import entities_DAO.PlaylistDAO;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author t00178764
+ */
+public class TestPlaylist {
+    public static void main(String[] args) {
+        PlaylistDAO playlistDAO = new PlaylistDAO();
+        
+        //TEST FOR FIND BY TNUMBER
+       /* Playlist playlist = playlistDAO.findByTnumber("T20000000");
+        System.out.println(playlist.getNickname()); */
+        
+        //TEST FOR CREATE
+        /*Playlist playlist = new Playlist("T20000002","nicknameplaylist3","password3","addrplaylist3@gmail.com");
+        System.out.println(playlistDAO.create(playlist).getTnumber());*/
+        
+        //TEST FOR UPDATE
+        /*Playlist playlist=new Playlist("T20000002","nicknameplaylist4","password4","addrplaylist4@gmail.com");
+        playlistDAO.update(playlist);*/
+        
+        //TEST FOR DELETE
+        /*Playlist playlist=new Playlist("T20000002","nicknameplaylist4","password4","addrplaylist4@gmail.com");
+        System.out.println(playlistDAO.delete(playlist).getTnumber());*/
+        
+        //TEST FOR FIND ALL
+        /* List<Playlist> playlists0 = playlistDAO.findAll();
+        for (Playlist playlist0 : playlists0) {
+            System.out.println(playlist0.getTnumber() + " - " + playlist0.getNickname() + " - " + playlist0.getPassword());
+    }*/
+    }
+}
+
