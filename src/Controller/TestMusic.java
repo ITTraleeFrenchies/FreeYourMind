432a108dba706d15c0532786089aa4985d030a4a
t00178747 <t00178747@1XX782BCBAC29F3.students.ittralee.local>
2014-12-01 10:41:41 +0000

    Test for Music written. Need to be tested.

--- src/Controller/TestMusic.java
+++ src/Controller/TestMusic.java
@@ -3,41 +3,43 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Controller;
 
 import entities.Music;
 import entities_DAO.MusicDAO;
+import java.util.List;
 
 /**
  *
  * @author t00178747
  */
 public class TestMusic {
-         MusicDAO musicDAO = new MusicDAO();
-    
-     // -------------- TEST FOR FIND BY IDTRACK ----------------
-        
-        Music music = musicDAO.find("M1");
-        
-          
-        
+
+    public static void main(String[] args) {
+        MusicDAO musicDAO = new MusicDAO();
+
+        // -------------- TEST FOR FIND BY IDTRACK ----------------
+        Music music = musicDAO.find("M61");
+        System.out.println(music.getIDTrack() + " - " + music.getTitle());
+
         // -------------- TEST FOR CREATE ----------------
-     /*  music = new Member("T00000012","nicknamemember3","password3",null,null,null,"addr3@gmail.com",null,null,null,null,null);
-         System.out.println(musicDAO.create(member).getTnumber());
-         */
+        music = new Music(null, null, "STupeflip vite", 1, null, null, null, null);
+        System.out.println(musicDAO.create(music).getIDTrack());
+
         // -------------- TEST FOR UPDATE ----------------
-        /*
-        music = musicDAO.findByTnumber("T00000012");
-        musicDAO.update(member);
-*/
+        
+         music = musicDAO.find("M61");
+         musicDAO.update(music);
+         
         // -------------- TEST FOR FINDALL ----------------
-       /* List<Music> members0 = musicDAO.findAll();
-        for (Music music : members0) {
-            System.out.println(member0.getTnumber() + " - " + member0.getNickname() + " - " + member0.getPassword());
-        }*/
-         // -------------- TEST FOR DELET ----------------
-      /*  Music music = musicDAO.findByTnumber("T00000012");
-        memberDAO.delete(member);
-      */
+        List<Music> listMusic = musicDAO.findAll();
+         for (Music aMusic : listMusic) {
+         System.out.println(aMusic.getIDTrack() + " - " + aMusic.getTitle()+ " - " + aMusic.getIDPlaylist());
+         }
+         
+        // -------------- TEST FOR DELETE ----------------
+         music = musicDAO.find("M61");
+         musicDAO.delete(music);
+         
+    }
 }
