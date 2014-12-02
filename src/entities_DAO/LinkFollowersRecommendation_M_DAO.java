b2248613467e066345a90aa5cec7bb3e7bb231be
t00178764 <t00178764@1XX782BCBAC2617.students.ittralee.local>
2014-12-02 17:27:39 +0000

    LinkFollowersRecommendation_P_DAO and LinkFollowersRecommendation_M_DAO updated Code Written by Quentin

--- src/entities_DAO/LinkFollowersRecommendation_M_DAO.java
+++ src/entities_DAO/LinkFollowersRecommendation_M_DAO.java
@@ -3,39 +3,152 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package entities_DAO;
 
 import entities.LinkFollowersRecommendation_M;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 /**
  *
  * @author t00178764
  */
 
-
-
 public class LinkFollowersRecommendation_M_DAO {
-       public LinkFollowersRecommendation_M find(long id) {
-        return null;
-    }
+	
+	private String nameDriver = "jdbc:oracle:thin:@cp3dbinstance.c4pxnpz4ojk8.us-east-1.rds.amazonaws.com:1521:cp3db";
+	private String username = "mm3";
+	private String password = "mm3";
+	private Connection connection;
+	private Statement statement;
+	private ResultSet resultSet;
+        
+	public LinkFollowersRecommendation_M_DAO(){
+    	
+	}
+	
+ 	public void openConnection() {
+            try {
+                    this.connection = DriverManager.getConnection(nameDriver, username, password);
+                    this.statement = connection.createStatement();
+            } catch (SQLException ex) {
+                    System.out.println(ex.getErrorCode() + " : error with the openning of  connection. ");
+            }
+	}
+        
+	public void closeConnection() {
+            try {
+                    if (this.connection != null && this.statement != null && this.resultSet != null) {
+                    this.connection.close();
+                    this.statement.close();
+                    this.resultSet.close();
+                    }
+            } catch (SQLException ex) {
+                    System.out.println(ex.getErrorCode() + " : error with the closing of connection. ");
+            }
+	}
+	
+	
+   	public LinkFollowersRecommendation_M find(long id) {
+    	
+            LinkFollowersRecommendation_M link = null;
+            String sql = "SELECT * FROM LINK_FOLLOWERS_RECOMMEND_M WHERE ID_LINK = '" + id + "'";
 
-    public LinkFollowersRecommendation_M create(LinkFollowersRecommendation_M link) {
-        return null;
-    }
+            openConnection();
+            try {
+                    this.resultSet = statement.executeQuery(sql);
+                    while (this.resultSet.next()) {
+                    link = new LinkFollowersRecommendation_M(
+                            this.resultSet.getInt("id_link"),
+                            this.resultSet.getInt("id_recommendation_music"),
+                            this.resultSet.getInt("id_followerlist")
+                    );
+                    }
+            } catch (SQLException ex) {
+                    System.out.println(ex.getErrorCode() + " error with the sql request.");
+            }
+            closeConnection();
+            return link;
+	}
+        
+	public LinkFollowersRecommendation_M create(LinkFollowersRecommendation_M linkToCreate) {
+    	
+            LinkFollowersRecommendation_M link = linkToCreate;
+            String sql = " INSERT INTO LINK_FOLLOWERS_RECOMMEND_P(ID_LINK,ID_RECOMMENDATION_MUSIC,ID_FOLLOWERLIST)"
+                    + "VALUES('" + linkToCreate.getId_link() + "','"
+                    + linkToCreate.getId_recommendation_music() + "','" + linkToCreate.getId_followerlist() + "')";
+            System.out.println(sql);
+            openConnection();
+            try {
+                    this.resultSet = statement.executeQuery(sql);
+            } catch (SQLException ex) {
+                    ex.printStackTrace();
+            }
+            closeConnection();
+            return link;
+	}
+        
+	public LinkFollowersRecommendation_M update(LinkFollowersRecommendation_M linkToUpdate) {
+    	
+            String sql = " UPDATE LINK_FOLLOWERS_RECOMMEND_M SET ID_LINK = '" + linkToUpdate.getId_link()
+                    + "', ID_RECOMMENDATION_MUSIC = '" + linkToUpdate.getId_recommendation_music()
+                    + "', ID_FOLLOWERLIST = '" + linkToUpdate.getId_followerlist()+ "'";
+            System.out.println(sql);
+            openConnection();
+            try {
+                    this.resultSet = statement.executeQuery(sql);
+            } catch (SQLException ex) {
+                    ex.printStackTrace();
+            }
+            closeConnection();
+            return linkToUpdate;
+	}
+        
+	public LinkFollowersRecommendation_M delete(LinkFollowersRecommendation_M linkToDelete) {
+    	
+            String sql = "DELETE FROM LINK_FOLLOWERS_RECOMMEND_M WHERE ID_LINK = '" + linkToDelete.getId_link() + "'";
+            System.out.println(sql);
 
-    public LinkFollowersRecommendation_M update(LinkFollowersRecommendation_M link) {
-        return null;
-    }
+            openConnection();
+            try {
+                    this.resultSet = statement.executeQuery(sql);
+            } catch (SQLException ex) {
+                    ex.printStackTrace();
+            }
+            
+            closeConnection();
+            return linkToDelete;
+	}
+        
+	public List<LinkFollowersRecommendation_M> findAll() {
+    	
+            List<LinkFollowersRecommendation_M> link_followers = new ArrayList();
+            LinkFollowersRecommendation_M link = null;
 
-    public void delete(LinkFollowersRecommendation_M link) {
-    }
+            String sql="SELECT * FROM LINK_FOLLOWERS_RECOMMEND_P";
+            openConnection();
+            try {
+                    this.resultSet = statement.executeQuery(sql);
+                    while (this.resultSet.next()) {
+                    link = new LinkFollowersRecommendation_M(
+                            this.resultSet.getInt("id_link"),
+                            this.resultSet.getInt("id_recommendation_music"),
+                            this.resultSet.getInt("id_followerlist")
+                            );
+                    link_followers.add(link);
+                    }
+            } catch (SQLException ex) {
+                    System.out.println(ex.getErrorCode() + " error with the sql request.");
+            }
+            closeConnection();
 
-    public List<LinkFollowersRecommendation_M> findAll() {
-        return null;
-    }
 
-    public List<LinkFollowersRecommendation_M> findById() {
-        return null;
-    }
+            return link_followers;
+	}
+	
 }
+
--- src/entities_DAO/LinkFollowersRecommendation_P_DAO.java
+++ src/entities_DAO/LinkFollowersRecommendation_P_DAO.java
@@ -3,39 +3,150 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package entities_DAO;
 
 import entities.LinkFollowersRecommendation_P;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 /**
  *
  * @author t00178764
  */
 
-
-
 public class LinkFollowersRecommendation_P_DAO {
-       public LinkFollowersRecommendation_P find(long id) {
-        return null;
-    }
+	
+	private String nameDriver = "jdbc:oracle:thin:@cp3dbinstance.c4pxnpz4ojk8.us-east-1.rds.amazonaws.com:1521:cp3db";
+	private String username = "mm3";
+	private String password = "mm3";
+	private Connection connection;
+	private Statement statement;
+	private ResultSet resultSet;
+        
+	public LinkFollowersRecommendation_P_DAO(){
+    	
+	}
+	
+ 	public void openConnection() {
+            try {
+                    this.connection = DriverManager.getConnection(nameDriver, username, password);
+                    this.statement = connection.createStatement();
+            } catch (SQLException ex) {
+                    System.out.println(ex.getErrorCode() + " : error with the openning of  connection. ");
+            }       
+	}
+        
+	public void closeConnection() {
+            try {
+                    if (this.connection != null && this.statement != null && this.resultSet != null) {
+                    this.connection.close();
+                    this.statement.close();
+                    this.resultSet.close();
+                    }
+            } catch (SQLException ex) {
+                    System.out.println(ex.getErrorCode() + " : error with the closing of connection. ");
+            }
+	}
+	
+   	public LinkFollowersRecommendation_P find(int id) {
+    	
+            LinkFollowersRecommendation_P link = null;
+            String sql = "SELECT * FROM LINK_FOLLOWERS_RECOMMEND_P WHERE ID_LINK = '" + id + "'";
 
-    public LinkFollowersRecommendation_P create(LinkFollowersRecommendation_P link) {
-        return null;
-    }
+            openConnection();
+            try {
+                    this.resultSet = statement.executeQuery(sql);
+                    while (this.resultSet.next()) {
+                    link = new LinkFollowersRecommendation_P(
+                            this.resultSet.getInt("id_link"),
+                            this.resultSet.getInt("id_recommendation_playlist"),
+                            this.resultSet.getInt("id_followerlist")
+                            );
+                    }
+            } catch (SQLException ex) {
+                    System.out.println(ex.getErrorCode() + " error with the sql request.");
+            }
+            closeConnection();
+            return link;
+	}
+        
+	public LinkFollowersRecommendation_P create(LinkFollowersRecommendation_P linkToCreate) {
+    	
+            LinkFollowersRecommendation_P link = linkToCreate;
+            String sql = " INSERT INTO LINK_FOLLOWERS_RECOMMEND_P(ID_LINK,ID_RECOMMENDATION_PLAYLIST,ID_FOLLOWERLIST)"
+                    + "VALUES('" + linkToCreate.getId_link() + "','"
+                    + linkToCreate.getId_recommendation_playlist() + "','" + linkToCreate.getId_followerlist() + "')";
+            System.out.println(sql);
+            openConnection();
+            try {
+                    this.resultSet = statement.executeQuery(sql);
+            } catch (SQLException ex) {
+                    ex.printStackTrace();
+            }
+            closeConnection();
+            return link;
+	}
+	public LinkFollowersRecommendation_P update(LinkFollowersRecommendation_P linkToUpdate) {
+    	
+            String sql = " UPDATE LINK_FOLLOWERS_RECOMMEND_P SET ID_LINK = '" + linkToUpdate.getId_link()
+                    + "', ID_RECOMMENDATION_PLAYLIST = '" + linkToUpdate.getId_recommendation_playlist()
+                    + "', ID_FOLLOWERLIST = '" + linkToUpdate.getId_followerlist()+ "'";
+            System.out.println(sql);
+            openConnection();
+            try {
+                    this.resultSet = statement.executeQuery(sql);
+            } catch (SQLException ex) {
+                    ex.printStackTrace();
+            }
+            closeConnection();
+            return linkToUpdate;
+	}
+        
+	public LinkFollowersRecommendation_P delete(LinkFollowersRecommendation_P linkToDelete) {
+    	
+            String sql = "DELETE FROM LINK_FOLLOWERS_RECOMMEND_P WHERE ID_LINK = '" + linkToDelete.getId_link() + "'";
+            System.out.println(sql);
 
-    public LinkFollowersRecommendation_P update(LinkFollowersRecommendation_P link) {
-        return null;
-    }
+            openConnection();
+            try {
+                    this.resultSet = statement.executeQuery(sql);
+            } catch (SQLException ex) {
+                    ex.printStackTrace();
+            }
+            
+            closeConnection();
+            
+            return linkToDelete;
+	}
+        
+	public List<LinkFollowersRecommendation_P> findAll() {
+    	
+            List<LinkFollowersRecommendation_P> link_followers = new ArrayList();
+            LinkFollowersRecommendation_P link = null;
 
-    public void delete(LinkFollowersRecommendation_P link) {
-    }
+            String sql="SELECT * FROM LINK_FOLLOWERS_RECOMMEND_P";
+            openConnection();
+            try {
+                    this.resultSet = statement.executeQuery(sql);
+                    while (this.resultSet.next()) {
+                    link = new LinkFollowersRecommendation_P(
+                            this.resultSet.getInt("id_link"),
+                            this.resultSet.getInt("id_recommendation_playlist"),
+                            this.resultSet.getInt("id_followerlist")
+                            );
+                    link_followers.add(link);
+                    }
+            } catch (SQLException ex) {
+                    System.out.println(ex.getErrorCode() + " error with the sql request.");
+            }
+            closeConnection();
 
-    public List<LinkFollowersRecommendation_P> findAll() {
-        return null;
-    }
 
-    public List<LinkFollowersRecommendation_P> findById() {
-        return null;
-    }
+            return link_followers;
+	}
+	
 }
