e4f67fa5369a5419adadfb56ead7c14f9eefe0fb
t00178764 <t00178764@1XX3417EBA79111.students.ittralee.local>
2014-12-11 16:00:11 +0000

    Constructor updated of followers

--- src/entities/Followers.java
+++ src/entities/Followers.java
@@ -22,7 +22,7 @@ public class Followers {
     }
 
     public Followers() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
     }
 
     public String getTnumber_follow() {
